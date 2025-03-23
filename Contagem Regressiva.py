a36f115803afcc612311ee60acb9ee27429fa578
L-Codex <sorteluc@gmail.com>
2025-03-23 14:04:48 -0300

    Add files via upload

---
+++ Contagem Regressiva.py
@@ -0,0 +1,10 @@
+# Solicita que o usuário insira um número inteiro positivo
+numero = int(input("Digite um número inteiro positivo para a contagem regressiva: "))
+
+# Verifica se o número é positivo
+if numero < 0:
+    print("Por favor, insira um número positivo.")
+else:
+    # Exibe a contagem regressiva
+    for i in range(numero, -1, -1):
+        print(i)
