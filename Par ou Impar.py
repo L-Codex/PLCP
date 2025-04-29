9de55ea435989f64f99eb79aee5071f11819c2e6
L-Codex <sorteluc@gmail.com>
2025-04-29 15:16:39 -0300

    Update Par ou Impar.py

--- Par ou Impar.py
+++ Par ou Impar.py
@@ -1,7 +1,5 @@
-# Solicita que o usuário insira um número inteiro
 numero = int(input("Digite um número inteiro: "))
 
-# Verifica se o número é par ou ímpar
 if numero % 2 == 0:
     print("O número é par.")
 else:
