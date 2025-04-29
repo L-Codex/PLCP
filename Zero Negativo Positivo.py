93b7f4bf133352fb2b211013d540c47c094a8f18
L-Codex <sorteluc@gmail.com>
2025-04-29 15:17:35 -0300

    Update Zero Negativo Positivo.py

--- Zero Negativo Positivo.py
+++ Zero Negativo Positivo.py
@@ -1,7 +1,5 @@
-# Solicita que o usuário insira um número
 numero = float(input("Digite um número: "))
 
-# Verifica se o número é positivo, negativo ou zero
 if numero > 0:
     print("O número é positivo.")
 elif numero < 0:
