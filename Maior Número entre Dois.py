afaeef6763d634a9b777cc7e0491ba63653ffdb7
L-Codex <sorteluc@gmail.com>
2025-04-29 15:19:47 -0300

    Update Maior Número entre Dois.py

--- Maior Número entre Dois.py
+++ Maior Número entre Dois.py
@@ -1,9 +1,17 @@
-numero1 = float(input("Digite o primeiro número: "))
-numero2 = float(input("Digite o segundo número: "))
+def maior_numero(numero1, numero2):
+    if numero1 > numero2:
+        return f"O maior número é: {numero1}"
+    elif numero2 > numero1:
+        return f"O maior número é: {numero2}"
+    else:
+        return "Os números são iguais."
 
-if numero1 > numero2:
-    print(f"O maior número é: {numero1}")
-elif numero2 > numero1:
-    print(f"O maior número é: {numero2}")
-else:
-    print("Os números são iguais.")
+def main():
+    numero1 = float(input("Digite o primeiro número: "))
+    numero2 = float(input("Digite o segundo número: "))
+    
+    resultado = maior_numero(numero1, numero2)
+    print(resultado)
+
+if __name__ == '__main__':
+    main()
