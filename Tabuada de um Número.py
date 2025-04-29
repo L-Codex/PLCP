67df860c475a298fff0acc6a801d6f7d43488f71
L-Codex <sorteluc@gmail.com>
2025-04-29 15:17:18 -0300

    Update Tabuada de um Número.py

--- Tabuada de um Número.py
+++ Tabuada de um Número.py
@@ -1,7 +1,5 @@
-# Solicita que o usuário insira um número
 numero = int(input("Digite um número para ver sua tabuada: "))
 
-# Exibe a tabuada do número
 print(f"\nTabuada de {numero}:")
 
 for i in range(1, 11):
