f7842da8eee4d9a524474f8034d561b806e9d7bb
L-Codex <sorteluc@gmail.com>
2025-04-29 15:21:12 -0300

    Update Ola.py

--- Ola.py
+++ Ola.py
@@ -2,7 +2,7 @@ def saudacao(nome):
     print(f"Olá, {nome}")
 
 def main():
-    nome = "Lucas"  # Você pode alterar o nome aqui se necessário
+    nome = "Lucas"
     saudacao(nome)
 
 if __name__ == '__main__':
